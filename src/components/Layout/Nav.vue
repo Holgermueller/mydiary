c2d45fbb60385bf5d31eaa3ec8f3efdc3a12253c
Holger Mueller <9jholger@gmail.com>
2020-11-12 16:12:02 -0600

    set up router scaffold main layout

---
+++ src/components/Layout/Nav.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="nav">
+    <v-app-bar fixed dense>
+      <div v-for="link in menuLinks" :key="link.title" link>
+        <v-btn :to="link.link">
+          <v-icon left>
+            {{ link.icon }}
+          </v-icon>
+          {{ link.title }}
+        </v-btn>
+      </div>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Nav",
+
+  data() {
+    return {};
+  },
+
+  computed: {
+    menuLinks() {
+      let menuLinks = [
+        {
+          icon: "mdi-train",
+          title: "Home",
+          link: "/",
+        },
+        {
+          icon: "mdi-account",
+          title: "Dashboard",
+          link: "/dashboard",
+        },
+      ];
+      return menuLinks;
+    },
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ src/firebase/fbInit.js
@@ -0,0 +1,6 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import fbConfig from "./fbConfig";
+const firebaseApp = firebase.initializeApp(fbConfig);
+export default firebaseApp.firestore();
