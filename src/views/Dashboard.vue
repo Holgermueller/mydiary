1fd88d9b6889d1a9d0a1a9d618b596d36296cdeb
Holger Mueller <9jholger@gmail.com>
2020-11-13 09:58:21 -0600

    began dash scaffold

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,6 +1,27 @@
 <template>
   <div>
-    <h1>This is the dash.</h1>
+    <v-card class="compose-card">
+      <v-card-title>
+        Write something here:
+      </v-card-title>
+
+      <v-card-text>
+        <v-form ref="form">
+          <v-text-field label="Title" v-model="title" outlined></v-text-field>
+
+          <v-textarea
+            label="Today's thoughts:"
+            v-model="todaysThoughts"
+          ></v-textarea>
+        </v-form>
+      </v-card-text>
+
+      <v-card-actions>
+        <v-btn>
+          Submit
+        </v-btn>
+      </v-card-actions>
+    </v-card>
   </div>
 </template>
 
@@ -8,6 +29,20 @@
 export default {
   name: "Dashboard",
 
+  data() {
+    return {
+      title: "",
+      todaysThoughts: "",
+    };
+  },
+
   components: {},
 };
 </script>
+
+<style scoped>
+.compose-card {
+  width: 75%;
+  margin: 10% auto;
+}
+</style>
