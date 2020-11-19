806a791b6d0e74b297d95a58ef052dbbcefdda9e
Holger Mueller <9jholger@gmail.com>
2020-11-19 11:14:30 -0600

    got data onto dom

--- src/store/entries/index.js
+++ src/store/entries/index.js
@@ -5,9 +5,38 @@ export default {
     entries: [],
   },
 
-  mutations: {},
+  mutations: {
+    SET_ENTRIES(state, payload) {
+      state.entries = payload;
+    },
+  },
 
   actions: {
+    getAllEntries({ commit }) {
+      commit("SET_LOADING", true);
+
+      firebase.collection("diaryEntries").onSnapshot(
+        (querySnapshot) => {
+          let entriesFromDB = [];
+          querySnapshot.forEach((doc) => {
+            let entryData = {
+              entryId: doc.id,
+              entryDate: doc.data().entryDate,
+              title: doc.data().title,
+              todaysThoughts: doc.data().todaysThoughts,
+            };
+            entriesFromDB.push(entryData);
+          });
+          commit("SET_ENTRIES", entriesFromDB);
+          commit("SET_LOADING", false);
+        },
+        (err) => {
+          commit("SET_LOADING", true);
+          commit("SET_ERROR", err);
+        }
+      );
+    },
+
     addEntry({ commit }, payload) {
       commit("SET_LOADING", true);
 
--- src/views/Previous.vue
+++ src/views/Previous.vue
@@ -1,11 +1,32 @@
 <template>
   <div id="previous">
+    <v-layout row v-if="error">
+      <v-flex xs12 sm12 md12 lg12 xl12>
+        <app-alert
+          @dismissed="onDismissed"
+          :text="error.message || error"
+        ></app-alert>
+      </v-flex>
+    </v-layout>
+
+    <v-layout class="progress">
+      <v-flex class="tet-xs-center">
+        <v-progress-circular
+          indeterminate
+          class="primary--text"
+          :width="7"
+          :size="70"
+          v-if="loading"
+        ></v-progress-circular>
+      </v-flex>
+    </v-layout>
+
     <v-card class="previous-entries">
       <v-expansion-panels>
-        <v-expansion-panel>
-          <v-expansion-panel-header>Some Date</v-expansion-panel-header>
+        <v-expansion-panel v-for="(entry, index) in getEntries" :key="index">
+          <v-expansion-panel-header>{{ entry.title }}</v-expansion-panel-header>
           <v-expansion-panel-content>
-            some content
+            {{ entry.todaysThoughts }}
           </v-expansion-panel-content>
         </v-expansion-panel>
       </v-expansion-panels>
@@ -16,6 +37,22 @@
 <script>
 export default {
   name: "Previous",
+
+  created() {
+    return this.$store.dispatch("getAllEntries");
+  },
+
+  computed: {
+    getEntries() {
+      return this.$store.getters.entries;
+    },
+    error() {
+      return this.$store.getters.error;
+    },
+    loading() {
+      return this.$store.getters.loading;
+    },
+  },
 };
 </script>
 
