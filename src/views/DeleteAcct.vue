aa5993fe2830641dc94444720009e333babed665
Holger Mueller <9jholger@gmail.com>
2020-12-25 15:11:47 -0600

    begin scaffold delete account page

---
+++ src/views/DeleteAcct.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="DeleteAcct">
+    <h1>page to delete account</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DeleteAcct",
+
+  computed: {},
+
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped></style>
